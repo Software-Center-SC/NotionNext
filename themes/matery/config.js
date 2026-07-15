36b3361fa2fea9cc2713515e36d1f7960a169d3f
Software-Center-SC <software-center@outlook.com>
2026-07-16 00:17:31 +0200

    Disable home banner in config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  MATERY_HOME_BANNER_ENABLE: true,
+  MATERY_HOME_BANNER_ENABLE: false,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   MATERY_HOME_BANNER_GREETINGS: [
     'Hi，我是一个程序员',

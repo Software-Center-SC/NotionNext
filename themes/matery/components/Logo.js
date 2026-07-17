8d2f01b8f468686035e6f86e61b93d6b15470972
Software-Center-SC <software-center@outlook.com>
2026-07-17 16:40:54 +0200

    Refactor Logo component for improved styling

--- themes/matery/components/Logo.js
+++ themes/matery/components/Logo.js
@@ -7,15 +7,14 @@ const Logo = props => {
   return (
     /
       <div className='flex items-center cursor-pointer space-x-3'>
-
-        <img
-          src='/logo.png'
+        logo.png'
           alt='Logo'
           className='h-10 w-auto'
         />
 
-        <div className='text-lg hover        </div>
-
+        <div className='text-lg hover:scale-110 transform duration-200'>
+          {siteInfo?.title || siteConfig('TITLE')}
+        </div>
       </div>
     </SmartLink>
   )

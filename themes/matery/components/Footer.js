1252b933cd32e5fd1312ff1e7fcb132d4f01d8b0
Software-Center-SC <software-center@outlook.com>
2026-07-18 19:12:53 +0200

    Refactor Footer component structure and links
    Updated footer layout and navigation links.
    style: rediseñar footer a un estilo minimalista
    
    - Se eliminó el bloque morado y las estadísticas (Busuanzi) para limpiar la interfaz.
    - Se simplificó el footer para mostrar únicamente el copyright y enlaces básicos.
    - Se configuró el fondo para que responda al modo claro/oscuro del tema.

--- themes/matery/components/Footer.js
+++ themes/matery/components/Footer.js
@@ -1,4 +1,3 @@
-import { BeiAnGongAn } from '@/components/BeiAnGongAn'
 import { siteConfig } from '@/lib/config'
 
 const Footer = ({ title }) => {
@@ -15,50 +14,23 @@ const Footer = ({ title }) => {
   })()
 
   return (
-    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-indigo-700 text-gray-300 justify-center text-center m-auto w-full leading-6  dark:text-gray-100 text-sm p-6'>
-      {/* <DarkModeButton/> */}
-      <i className='fas fa-copyright' /> {`${copyrightDate}`}{' '}
-      <span>
-        <span className='w-5 mx-1 text-center'>
-          <i className=' animate-pulse fas fa-heart' />
-        </span>{' '}
-        <a
-          href={siteConfig('LINK')}
-          className='underline font-bold  dark:text-gray-300 '>
-          {siteConfig('AUTHOR')}
-        </a>
-        .<br />
-        {siteConfig('BEI_AN') && (
-          <>
-            <i className='fas fa-shield-alt' />{' '}
-            <a href={siteConfig('BEI_AN_LINK')} className='mr-2'>
-              {siteConfig('BEI_AN')}
-            </a>
-            <br />
-          </>
-        )}
-        <BeiAnGongAn />
-        <span className='hidden busuanzi_container_site_pv'>
-          <i className='fas fa-eye' />
-          <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
-        </span>
-        <span className='pl-2 hidden busuanzi_container_site_uv'>
-          <i className='fas fa-users' />{' '}
-          <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
-        </span>
-        <br />
-        <h1>{title}</h1>
-        <span className='text-xs '>
-          Powered by{' '}
-          <a
-            href='https://github.com/tangly1024/NotionNext'
-            className='underline dark:text-gray-300'>
-            NotionNext {siteConfig('VERSION')}
+    <footer className='relative z-10 w-full bg-white dark:bg-hexo-black-gray border-t dark:border-black py-8 mt-8'>
+      <div className='text-center text-gray-500 dark:text-gray-400 text-sm'>
+        {/* Copyright */}
+        <p>
+          &copy; {copyrightDate} {siteConfig('AUTHOR') || title}. Todos los derechos reservados.
+        </p>
+        
+        {/* Enlaces básicos de navegación */}
+        <div className='mt-2 space-x-4'>
+          <a href="/" className='hover:text-indigo-600 dark:hover:text-indigo-400 transition-colors'>
+            Inicio
           </a>
-          .
-        </span>
-      </span>
-      <br />
+          <a href="/archive" className='hover:text-indigo-600 dark:hover:text-indigo-400 transition-colors'>
+            Archivo
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
